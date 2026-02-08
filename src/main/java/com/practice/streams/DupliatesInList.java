e7ca6b36b9b388e1aa21d62b7cf9f88a9ef24599
Tushar <tusharmahajan762@gmail.com>
2026-02-08 23:37:43 +0530

    Set in streams

--- src/main/java/com/practice/streams/DupliatesInList.java
+++ src/main/java/com/practice/streams/DupliatesInList.java
@@ -1,18 +1,21 @@
 package com.practice.streams;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import static java.lang.reflect.Array.set;
 
 public class DupliatesInList
 {
-
     public static void main(String[] args)
     {
-
         List<String> list=List.of("aman","aman","raman","agaam","ram");
+        Set<String> set=new HashSet<>();
+        Set<String> newList= list.stream().
+                filter(x->!set.add(x)).collect(Collectors.toSet());
 
-
+        System.out.println(" New fliterd list is "+newList);
     }
-
 }
